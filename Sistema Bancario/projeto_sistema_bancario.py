083175c683d4c74811af3c7a6b24d2fd0dd8c6cc
Manuela França <manuelaalves26@gmail.com>
2024-09-19 18:05:48 -0300

    Implementando a operação de depósito.

--- Sistema Bancario/projeto_sistema_bancario.py
+++ Sistema Bancario/projeto_sistema_bancario.py
@@ -21,3 +21,13 @@ while True:
     print("3. Extrato")
     print("4. Sair")
     opcao = input("Escolha uma opção: ")
+
+    if opcao == '1':  # Depósito
+        valor_deposito = float(input("Digite o valor do depósito: R$ "))
+        if valor_deposito > 0:
+            saldo += valor_deposito
+            depositos.append(valor_deposito)
+            print(f"""Depósito de R$ {valor_deposito:.2f}
+                  realizado com sucesso.""")
+        else:
+            print("Valor de depósito inválido. Insira um valor positivo.")
