fdf10e1758f0723dee549a2bf028b8bd4b06a33d
Manuela França <manuelaalves26@gmail.com>
2024-09-19 17:53:31 -0300

    Inicializando as variáveis e implementando o menu no loop while.

--- Sistema Bancario/projeto_sistema_bancario.py
+++ Sistema Bancario/projeto_sistema_bancario.py
@@ -6,3 +6,18 @@ menu = """
 [4] Sair
 
 => """
+
+saldo = 0.0
+depositos = []
+saques = []
+limite_saque_diario = 3
+saque_maximo = 500.0
+saques_diarios_realizados = 0
+
+while True:
+    print("----- Sistema Bancário -----")
+    print("1. Depositar")
+    print("2. Sacar")
+    print("3. Extrato")
+    print("4. Sair")
+    opcao = input("Escolha uma opção: ")
