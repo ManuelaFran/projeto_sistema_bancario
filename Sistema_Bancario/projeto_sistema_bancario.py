d3fd7c4183af53966ab1085cb236bc2960fc052f
Manuela França <manuelaalves26@gmail.com>
2024-09-25 15:30:15 -0300

    Implementando a função de exibir extrato.

--- Sistema_Bancario/projeto_sistema_bancario.py
+++ Sistema_Bancario/projeto_sistema_bancario.py
@@ -63,7 +63,18 @@ def deposito(saldo, valor, extrato, /):
     return saldo, extrato
 
 
-saldo = 0.0
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n--- Extrato da Conta ---")
+    if len(extrato) == 0:
+        print("Nenhuma transação realizada.")
+    else:
+        for transacao in extrato:
+            print(transacao)
+    print(f"Saldo atual: R$ {saldo:.2f}")
+    print("------------------------\n")
+
+
+"""saldo = 0.0
 depositos = []
 saques = []
 limite_saque_diario = 3
@@ -77,51 +88,4 @@ while True:
     print("3. Extrato")
     print("4. Sair")
     opcao = input("Escolha uma opção: ")
-
-    if opcao == '1':  # Depósito
-        valor_deposito = float(input("Digite o valor do depósito: R$ "))
-        if valor_deposito > 0:
-            saldo += valor_deposito
-            depositos.append(valor_deposito)
-            print(f"""Depósito de R$ {valor_deposito:.2f}
-                  realizado com sucesso.""")
-        else:
-            print("Valor de depósito inválido. Insira um valor positivo.")
-
-    elif opcao == '2':  # Saque
-        if saques_diarios_realizados >= limite_saque_diario:
-            print("Você já realizou o limite máximo de saques diários.")
-        else:
-            valor_saque = float(input("Digite o valor do saque: R$ "))
-            if valor_saque > saldo:
-                print("Saldo insuficiente para realizar o saque.")
-            elif valor_saque > saque_maximo:
-                print(f"""Valor do saque excede o limite de R$
-                      {saque_maximo:.2f} por saque.""")
-            elif valor_saque > 0:
-                saldo -= valor_saque
-                saques.append(valor_saque)
-                saques_diarios_realizados += 1
-                print(f"Saque de R$ {valor_saque:.2f} realizado com sucesso.")
-            else:
-                print("Valor de saque inválido. Insira um valor positivo.")
-
-    elif opcao == '3':  # Extrato
-        print("\n--- Extrato da Conta ---")
-        if len(depositos) == 0 and len(saques) == 0:
-            print("Nenhuma transação realizada.")
-        else:
-            print("Depósitos:")
-            for deposito in depositos:
-                print(f"R$ {deposito:.2f}")
-            print("\nSaques:")
-            for saque in saques:
-                print(f"R$ {saque:.2f}")
-
-        print(f"\nSaldo atual: R$ {saldo:.2f}")
-        print("------------------------\n")
-    elif opcao == '4':  # Sair
-        print("Encerrando sistema bancário...")
-        break
-    else:
-        print("Opção inválida. Tente novamente.")
+"""
