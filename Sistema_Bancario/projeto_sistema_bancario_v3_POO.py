a90f1855c656d94aa837ded29f3ebb18258715a1
Manuela França <manuelaalves26@gmail.com>
2024-10-08 12:22:21 -0300

    Implementação da classe Deposito que herda a classe abstrata, ou seja, a Interface Transacao e implementa o método obrigatório "registrar".

--- Sistema_Bancario/projeto_sistema_bancario_v3_POO.py
+++ Sistema_Bancario/projeto_sistema_bancario_v3_POO.py
@@ -11,3 +11,11 @@ class Transacao(ABC):
 class Deposito(Transacao):
     def __init__(self, valor):
         self._valor = valor
+
+    def registrar(self, conta):
+        if self._valor > 0:
+            conta._saldo += self._valor
+            conta._historico.adicionar_transacao(f"""Depósito: R$ {
+                self._valor:.2f} - {datetime.now()}""")
+        else:
+            print("Valor de depósito inválido.")
