62cc0cc70390b991a7d3ea685eeae52d4fc6ed83
Manuela França <manuelaalves26@gmail.com>
2024-10-08 15:15:57 -0300

    Implementação da classe Saque que herda a classe abstrata, ou seja, a Interface Transacao e implementa o método obrigatório "registrar".

--- Sistema_Bancario/projeto_sistema_bancario_v3_POO.py
+++ Sistema_Bancario/projeto_sistema_bancario_v3_POO.py
@@ -19,3 +19,22 @@ class Deposito(Transacao):
                 self._valor:.2f} - {datetime.now()}""")
         else:
             print("Valor de depósito inválido.")
+
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    def registrar(self, conta):
+        if self._valor > conta._saldo:
+            print("Saldo insuficiente")
+        elif self._valor > conta._limite:
+            print(f"Valor do saque excede o limite de R$ {conta._limite:.2f}.")
+        elif conta._limite_saques <= 0:
+            print("Limite de saques diários atingido.")
+        else:
+            conta._saldo -= self._valor
+            conta._limite_saques -= 1
+            conta._historico.adicionar_transacao(f"""Saque: R$ {
+                self._valor:.2f} - {datetime.now()}""")
+            print(f"Saque de R$ {self._valor:.2f} realizado com sucesso.")
